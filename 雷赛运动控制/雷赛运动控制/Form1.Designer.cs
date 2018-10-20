9b2d877a96f4119ab138a82e93cb9af53af3c10b
mumushui <13035857149@sina.cn>
2018-10-20 17:50:13 +0800

    firstrcommit
    所有的东西都是初始的。

---
+++ 雷赛运动控制/雷赛运动控制/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace 雷赛运动控制
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OrgBtn = new System.Windows.Forms.Button();
+            this.CheckBtn = new System.Windows.Forms.Button();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.EmgStopBtn = new System.Windows.Forms.Button();
+            this.DisBox0 = new System.Windows.Forms.TextBox();
+            this.DisBox1 = new System.Windows.Forms.TextBox();
+            this.DisBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PosBox0 = new System.Windows.Forms.TextBox();
+            this.PosBox1 = new System.Windows.Forms.TextBox();
+            this.PosBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OrgBtn
+            // 
+            this.OrgBtn.Location = new System.Drawing.Point(419, 41);
+            this.OrgBtn.Name = "OrgBtn";
+            this.OrgBtn.Size = new System.Drawing.Size(96, 51);
+            this.OrgBtn.TabIndex = 0;
+            this.OrgBtn.Text = "回零";
+            this.OrgBtn.UseVisualStyleBackColor = true;
+            this.OrgBtn.Click += new System.EventHandler(this.OrgBtn_Click);
+            // 
+            // CheckBtn
+            // 
+            this.CheckBtn.Location = new System.Drawing.Point(419, 119);
+            this.CheckBtn.Name = "CheckBtn";
+            this.CheckBtn.Size = new System.Drawing.Size(96, 46);
+            this.CheckBtn.TabIndex = 0;
+            this.CheckBtn.Text = "检测io";
+            this.CheckBtn.UseVisualStyleBackColor = true;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(419, 185);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(96, 46);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "运动";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(419, 251);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(96, 46);
+            this.StopBtn.TabIndex = 0;
+            this.StopBtn.Text = "停止";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            // 
+            // EmgStopBtn
+            // 
+            this.EmgStopBtn.BackColor = System.Drawing.Color.Red;
+            this.EmgStopBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.EmgStopBtn.Location = new System.Drawing.Point(23, 23);
+            this.EmgStopBtn.Name = "EmgStopBtn";
+            this.EmgStopBtn.Size = new System.Drawing.Size(142, 176);
+            this.EmgStopBtn.TabIndex = 1;
+            this.EmgStopBtn.Text = "紧急停止";
+            this.EmgStopBtn.UseVisualStyleBackColor = false;
+            this.EmgStopBtn.Click += new System.EventHandler(this.EmgStopBtn_Click);
+            // 
+            // DisBox0
+            // 
+            this.DisBox0.Location = new System.Drawing.Point(307, 199);
+            this.DisBox0.Name = "DisBox0";
+            this.DisBox0.Size = new System.Drawing.Size(100, 21);
+            this.DisBox0.TabIndex = 2;
+            // 
+            // DisBox1
+            // 
+            this.DisBox1.Location = new System.Drawing.Point(307, 244);
+            this.DisBox1.Name = "DisBox1";
+            this.DisBox1.Size = new System.Drawing.Size(100, 21);
+            this.DisBox1.TabIndex = 2;
+            // 
+            // DisBox3
+            // 
+            this.DisBox3.Location = new System.Drawing.Point(307, 279);
+            this.DisBox3.Name = "DisBox3";
+            this.DisBox3.Size = new System.Drawing.Size(100, 21);
+            this.DisBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0轴运动距离";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "1轴运动距离";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3轴运动距离";
+            // 
+            // PosBox0
+            // 
+            this.PosBox0.Location = new System.Drawing.Point(307, 57);
+            this.PosBox0.Name = "PosBox0";
+            this.PosBox0.Size = new System.Drawing.Size(100, 21);
+            this.PosBox0.TabIndex = 2;
+            // 
+            // PosBox1
+            // 
+            this.PosBox1.Location = new System.Drawing.Point(307, 102);
+            this.PosBox1.Name = "PosBox1";
+            this.PosBox1.Size = new System.Drawing.Size(100, 21);
+            this.PosBox1.TabIndex = 2;
+            // 
+            // PosBox3
+            // 
+            this.PosBox3.Location = new System.Drawing.Point(307, 137);
+            this.PosBox3.Name = "PosBox3";
+            this.PosBox3.Size = new System.Drawing.Size(100, 21);
+            this.PosBox3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(230, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "0轴位置";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(230, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "1轴位置";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(230, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "3轴位置";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 326);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PosBox3);
+            this.Controls.Add(this.DisBox3);
+            this.Controls.Add(this.PosBox1);
+            this.Controls.Add(this.DisBox1);
+            this.Controls.Add(this.PosBox0);
+            this.Controls.Add(this.DisBox0);
+            this.Controls.Add(this.EmgStopBtn);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.CheckBtn);
+            this.Controls.Add(this.OrgBtn);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OrgBtn;
+        private System.Windows.Forms.Button CheckBtn;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Button EmgStopBtn;
+        private System.Windows.Forms.TextBox DisBox0;
+        private System.Windows.Forms.TextBox DisBox1;
+        private System.Windows.Forms.TextBox DisBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PosBox0;
+        private System.Windows.Forms.TextBox PosBox1;
+        private System.Windows.Forms.TextBox PosBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ 雷赛运动控制/雷赛运动控制/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using csLTDMC;
+
+namespace 雷赛运动控制
+{
+    public partial class Form1 : Form
+    {
+        UInt16 CardNo =0;//定义控制卡卡号
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         short n;
+         UInt16 i;//定义轴数
+
+
+
+        n = LTDMC.dmc_board_init() ;                 //控制卡的初始化操作，调用后必须使用dmc_board_close关闭卡。
+        //中间不可再次调用该初始化函数。
+        LTDMC.dmc_board_reset();
+        if (n <= 0)            //正常的卡数在1- 8之间
+            {
+                MessageBox.Show("初始化运动控制卡失败,请关闭其他运动控制卡程序");
+            }
+        //else if(n>8)
+        //{
+        //        MessageBox.Show("运动控制卡过多，请拔除多余的运动控制卡");
+        //}
+        
+      
+        for ( i=0;i<4;i++)
+            {  LTDMC.dmc_set_pulse_outmode(CardNo, i, 0) ;    //设定脉冲输出模式
+               
+            }
+        
+
+
+        }
+
+        private void EmgStopBtn_Click(object sender, EventArgs e)
+        {
+            LTDMC.dmc_emg_stop(CardNo);
+        }
+
+        private void OrgBtn_Click(object sender, EventArgs e)
+        {
+            UInt16 Pos0, Pos1, Pos3;
+            UInt16 HomeDir1, HomeDir0, HomeDir3; 
+            if (PosBox0.Text == null | PosBox1.Text == null | PosBox3.Text == null)
+            {
+                MessageBox.Show("请输入三轴相对原点感应器的位置");
+            }
+            else
+            {
+                
+                UInt16.TryParse(PosBox0.Text, out Pos0);
+                UInt16.TryParse(PosBox1.Text, out Pos1);
+                UInt16.TryParse(PosBox3.Text, out Pos3);
+                if (Pos0 > 0)
+                {
+                    HomeDir0 = 0;
+                }
+                else
+                {
+                    HomeDir0 = 1;
+
+
+                }
+
+                if (Pos1 > 0)
+                {
+                    HomeDir1 = 0;
+                }
+                else
+                {
+                    HomeDir1 = 1;
+                }
+                if (Pos3 > 0)
+                {
+                    HomeDir3 = 0;
+                }
+                else
+                {
+                    HomeDir3 = 1;
+                }
+                LTDMC.dmc_set_homemode(CardNo, 0, HomeDir0, 0, 0, 0);
+                LTDMC.dmc_set_homemode(CardNo, 1, HomeDir1, 0, 0, 0 );
+                LTDMC.dmc_set_homemode(CardNo, 3, HomeDir3, 0, 0, 0);
+                LTDMC.dmc_set_profile(CardNo, 0, 500, 1000, 0.1, 0.1, 500);
+                LTDMC.dmc_home_move(CardNo,0);
+                LTDMC.dmc_set_pulse_outmode(CardNo, 0, 0); 
+            }
+            
+            }
+
+        
+    }
+} 
